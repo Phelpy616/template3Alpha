4f5b5558e9428b3aca40690dce4e068a1c3c57f0
Cristian Mcknight <cristianmcknight@Cristians-MacBook-Air.local>
2025-03-22 18:58:33 -0300

    updates

--- goToHome.js
+++ goToHome.js
@@ -2,3 +2,9 @@ const logo = document.querySelector(".header .logo");
 logo.addEventListener("click", function () {
   window.location.href = logo.dataset.url;
 });
+
+//go to ConstellarWeb page
+const goBackBtn = document.querySelector('.tag div')
+goBackBtn.addEventListener('click',()=>{
+  window.location.href = 'https://constellarweb.com/'
+})
